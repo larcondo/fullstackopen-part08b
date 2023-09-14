342d9ca6cebed7bdee7fd72fe879fb0459d7d7d7
larcondo <larcondo90@gmail.com>
2023-09-14 14:59:39 -0300

    add error handler to Person Form

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import { useMutation } from '@apollo/client'
 import { CREATE_PERSON, ALL_PERSONS } from '../queries'
 
-const PersonForm = () => {
+const PersonForm = ({ setError }) => {
   const [name, setName] = useState('')
   const [phone, setPhone] = useState('')
   const [street, setStreet] = useState('')
   const [city, setCity] = useState('')
 
   const [createPerson] = useMutation(CREATE_PERSON, {
-    refetchQueries: [ { query: ALL_PERSONS } ]
+    refetchQueries: [ { query: ALL_PERSONS } ],
+    onError: (error) => {
+      const messages = error.graphQLErrors[0].message
+      setError(messages)
+    }
   })
 
   const submit = (event) => {
