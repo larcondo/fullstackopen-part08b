2b7e1509db8df1ecdae025467a3b7d813032857e
larcondo <larcondo90@gmail.com>
2023-09-14 12:31:08 -0300

    making queries from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { gql, useQuery } from '@apollo/client'
+import Persons from './components/Persons'
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`
 
 function App() {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
 
   return (
-    <div>
-      <h1>Vite + React</h1>
-    </div>
+    <Persons persons={result.data.allPersons} />
   )
 }
 
